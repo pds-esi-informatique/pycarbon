2d3f1051d206da7085b108c3887fc7ce9db88488
pds <pds@esi-informatique.com>
2021-11-09 16:37:59 +0100

    add functions : to date time string, date string, atom string, iso8601 string

--- pycarbon/pycarbon.py
+++ pycarbon/pycarbon.py
@@ -1,11 +1,10 @@
 import datetime
 class PyCarbon():
     def __init__(self):
-        self.datetime_today = ''
+        self.datetime_today = None
     def __str__(self):
         if self.datetime_today and type(self.datetime_today) is datetime.datetime:
             return self.datetime_today.strftime("%Y-%m-%d, %H:%M:%S")
-        return self.datetime_today
     def now(self):
         self.datetime_today = datetime.datetime.today()
         return self.datetime_today
@@ -21,4 +20,11 @@ class PyCarbon():
     def sub_day(self):
         self.datetime_today = self.datetime_today - datetime.timedelta(days=1)
         return self.datetime_today
-
+    def to_date_time_string(self):
+        return self.datetime_today.strftime("%Y-%m-%d, %H:%M:%S")
+    def to_date_string(self):
+        return self.datetime_today.strftime("%Y-%m-%d")
+    def to_atom_string(self):
+        return self.datetime_today.astimezone().replace(tzinfo=datetime.timezone.utc).isoformat()
+    def to_iso8601_string(self):
+        return self.datetime_today.astimezone().replace(microsecond=0).isoformat()
