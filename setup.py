1dff2df0e1b0702565d9aee9d7e5d31ce6547096
pds <pds@esi-informatique.com>
2021-11-09 15:49:24 +0100

    first functions in pycarbon

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pycarbon',
+    version='0.1',
+    packages=['pycarbon','tests'],
+    url='https://github.com/pds-esi-informatique/pycarbon',
+    license='MIT',
+    author='Pascal de Sélys',
+    author_email='pds@esi-informatique.com',
+    description='Copy of the excellent carbon library in php for python'
+)
