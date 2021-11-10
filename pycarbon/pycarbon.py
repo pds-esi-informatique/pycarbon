887a075680836d24a19d1cbac167bd9edb9a40a9
pds <pds@esi-informatique.com>
2021-11-10 13:55:03 +0100

    refactor code

--- pycarbon/pycarbon.py
+++ pycarbon/pycarbon.py
@@ -1,31 +1,20 @@
 import datetime
 import calendar
 
-class PyCarbon():
+
+class PyCarbon:
     def __init__(self):
         self.datetime_today = None
+
     def __str__(self):
         if self.datetime_today and type(self.datetime_today) is datetime.datetime:
             return self.datetime_today.strftime("%Y-%m-%d, %H:%M:%S")
+
     def __repr__(self):
         if self.datetime_today and type(self.datetime_today) is datetime.datetime:
             return self.datetime_today.strftime("%Y-%m-%d, %H:%M:%S")
-    def now(self):
-        self.datetime_today = datetime.datetime.today()
-        return self
-    def add_days(self,days:int):
-        self.datetime_today = self.datetime_today + datetime.timedelta(days=days)
-        return self
-    def add_day(self):
-        self.datetime_today = self.datetime_today + datetime.timedelta(days=1)
-        return self
-    def sub_days(self,days:int):
-        self.datetime_today = self.datetime_today - datetime.timedelta(days=days)
-        return self
-    def sub_day(self):
-        self.datetime_today = self.datetime_today - datetime.timedelta(days=1)
-        return self
-    def _calculate_month(self,months:int):
+
+    def _calculate_month(self, months: int):
         """
         https://stackoverflow.com/questions/4130922/how-to-increment-datetime-by-custom-months-in-python-without-using-library/23506665
         """
@@ -34,29 +23,59 @@ class PyCarbon():
         month = month % 12 + 1
         day = min(self.datetime_today.day, calendar.monthrange(year, month)[1])
         return self.datetime_today.replace(year=year, month=month, day=day)
+
+    def now(self):
+        self.datetime_today = datetime.datetime.today()
+        return self
+
+    def add_days(self, days: int):
+        self.datetime_today = self.datetime_today + datetime.timedelta(days=days)
+        return self
+
+    def add_day(self):
+        self.datetime_today = self.datetime_today + datetime.timedelta(days=1)
+        return self
+
+    def sub_days(self, days: int):
+        self.datetime_today = self.datetime_today - datetime.timedelta(days=days)
+        return self
+
+    def sub_day(self):
+        self.datetime_today = self.datetime_today - datetime.timedelta(days=1)
+        return self
+
     def add_month(self):
         self.datetime_today = self._calculate_month(1)
         return self
-    def add_months(self,months:int):
+
+    def add_months(self, months: int):
         self.datetime_today = self._calculate_month(months)
         return self
+
     def sub_month(self):
         self.datetime_today = self._calculate_month(1)
         return self
-    def sub_months(self,months:int):
+
+    def sub_months(self, months: int):
         self.datetime_today = self._calculate_month(months)
         return self
+
     def first_of_month(self):
         self.datetime_today = self.datetime_today.replace(day=1)
         return self
+
     def start_of_month(self):
-        self.datetime_today = self.datetime_today.replace(day=1,hour=0,minute=0,second=0)
+        self.datetime_today = self.datetime_today.replace(day=1, hour=0, minute=0, second=0)
         return self
+
     def to_date_time_string(self):
         return self.datetime_today.strftime("%Y-%m-%d, %H:%M:%S")
+
     def to_date_string(self):
         return self.datetime_today.strftime("%Y-%m-%d")
+
     def to_atom_string(self):
         return self.datetime_today.astimezone().replace(tzinfo=datetime.timezone.utc).isoformat()
+
     def to_iso8601_string(self):
         return self.datetime_today.astimezone().replace(microsecond=0).isoformat()
