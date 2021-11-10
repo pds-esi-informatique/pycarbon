4b5889e1cd8573c2d75040e1b21b860539dc3640
pds <pds@esi-informatique.com>
2021-11-10 14:54:12 +0100

    refactor code, add abs for check number is positive, remodify to class function protected for month day

--- pycarbon/pycarbon.py
+++ pycarbon/pycarbon.py
@@ -14,50 +14,63 @@ class PyCarbon:
         if self.datetime_today and type(self.datetime_today) is datetime.datetime:
             return self.datetime_today.strftime("%Y-%m-%d, %H:%M:%S")
 
-    def _calculate_month(self, months: int):
+    @staticmethod
+    def _positive_parameter_number(number):
+        return abs(number)
+
+    @staticmethod
+    def _calculate_months(datetime_today, months: int):
         """
         https://stackoverflow.com/questions/4130922/how-to-increment-datetime-by-custom-months-in-python-without-using-library/23506665
         """
-        month = self.datetime_today.month - 1 + months
-        year = self.datetime_today.year + month // 12
+        month = datetime_today.month - 1 + months
+        year = datetime_today.year + month // 12
         month = month % 12 + 1
-        day = min(self.datetime_today.day, calendar.monthrange(year, month)[1])
-        return self.datetime_today.replace(year=year, month=month, day=day)
+        day = min(datetime_today.day, calendar.monthrange(year, month)[1])
+        return datetime_today.replace(year=year, month=month, day=day)
+
+    @staticmethod
+    def _calculate_days(datetime_today, days: int):
+        return datetime_today + datetime.timedelta(days=days)
 
     def now(self):
         self.datetime_today = datetime.datetime.today()
         return self
 
-    def add_days(self, days: int):
-        self.datetime_today = self.datetime_today + datetime.timedelta(days=days)
-        return self
-
     def add_day(self):
-        self.datetime_today = self.datetime_today + datetime.timedelta(days=1)
+        self.datetime_today = self._calculate_days(self.datetime_today, 1)
         return self
 
-    def sub_days(self, days: int):
-        self.datetime_today = self.datetime_today - datetime.timedelta(days=days)
+    def add_days(self, days: int):
+        days = self._positive_parameter_number(days)
+        self.datetime_today = self._calculate_days(self.datetime_today, days)
         return self
 
     def sub_day(self):
-        self.datetime_today = self.datetime_today - datetime.timedelta(days=1)
+        self.datetime_today = self._calculate_days(self.datetime_today, -1)
+        return self
+
+    def sub_days(self, days: int):
+        days = self._positive_parameter_number(days)
+        self.datetime_today = self._calculate_days(self.datetime_today, -days)
         return self
 
     def add_month(self):
-        self.datetime_today = self._calculate_month(1)
+        self.datetime_today = self._calculate_months(self.datetime_today, 1)
         return self
 
     def add_months(self, months: int):
-        self.datetime_today = self._calculate_month(months)
+        months = self._positive_parameter_number(months)
+        self.datetime_today = self._calculate_months(self.datetime_today, months)
         return self
 
     def sub_month(self):
-        self.datetime_today = self._calculate_month(1)
+        self.datetime_today = self._calculate_months(self.datetime_today, -1)
         return self
 
     def sub_months(self, months: int):
-        self.datetime_today = self._calculate_month(months)
+        months = self._positive_parameter_number(months)
+        self.datetime_today = self._calculate_months(self.datetime_today, -months)
         return self
 
     def first_of_month(self):
